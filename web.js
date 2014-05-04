177a5f913ebb7e63279ef9874950048fdbadda74
Matti Luukkainen <mluukkai@iki.fi>
2014-05-04 15:57:37 +0300

    joko ois

--- web.js
+++ web.js
@@ -5,12 +5,6 @@ var app = express();
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-app.get('/', function(request, response) {
-    response.sendfile(__dirname + '../dist/index.html');
-}).configure(function() {
-    app.use('/', express.static(__dirname + '/dist/'));
-});
-
 app.listen(process.env.PORT || 5000);
 
 
