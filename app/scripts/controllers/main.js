b026615ec5a55ee67c7e9bbd0267f098eaf4f87a
Matti Luukkainen <mluukkai@iki.fi>
2014-05-06 18:00:56 +0300

    auth added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,35 +20,108 @@ app.directive('flash', function() {
   };
 });
 
+app.factory('Auth', function($http){
+    var URL = 'http://ng-project-backend.herokuapp.com/session'; 
+    var service = {};
+
+    service.logged = {}; 
+
+    service.do = function() {
+      console.log("do")
+      return $http.get(URL)
+    }
+
+    service.login = function(credentials) {
+      return $http.post(URL, credentials).then(
+        function (token) {
+          service.logged.status = true;
+          service.logged.user = credentials.user;
+          $http.defaults.headers.common['auth-token'] = token.data;
+          return token.data;
+        }
+      )  
+    } 
+
+    service.logout = function(credentials) {
+      return $http.delete(URL).then(
+        function () {
+          service.logged.status = false;
+          service.logged.user = null;
+          delete $http.defaults.headers.common['auth-token']
+          return null;
+        }
+      )
+    } 
+
+    return service;
+});
+
 app.factory('Blogs', function($http){
-    var URL = 'http://ng-project-backend.herokuapp.com/api/blogs.json'; 
+    var URL_BASE = 'http://ng-project-backend.herokuapp.com/api/blogs'; 
+
     var blogsService = {};
     
     blogsService.all = function(){
-    	return $http.get(URL)	
+    	return $http.get(URL_BASE+".json")	
     } 
 
     blogsService.create = function(data){
     	console.log("called")
-    	return $http.post(URL, data)	
+    	return $http.post(URL_BASE+".json", data)	
+    } 
+
+    blogsService.delete = function(data){
+      return $http.delete(URL_BASE+"/"+data.id+".json", data) 
     } 
 
     return blogsService;
- 
 });
 
-app.controller('MainCtrl', function ($scope, Blogs) {
-	$scope.formVisible = false;
+app.controller('MainCtrl', function ($scope, $http, Blogs, Auth) {
+    $scope.formVisible = false;
+    $scope.loggedIn = Auth.logged;
 
     Blogs.all().success( function(data, status, headers, config) {
     	$scope.entries = data;
     }); 
 
+    $scope.do = function(){
+      Auth.do().then(
+        function (d) {
+          console.log(d.data)
+        } 
+      )
+    }
+
+    $scope.deleteBlog = function(entry) { 
+      Blogs.delete(entry).success(function(){
+        var index = $scope.entries.indexOf(entry)
+        $scope.entries.splice(index, 1);
+      });
+    }
+
+    $scope.logout = function() {
+      Auth.logout()
+    }
+
+    $scope.login = function(){
+      Auth.login($scope.credentials)
+      .then( 
+        function(data) {
+          // success
+        },function( data ) {
+          // failure
+        }
+      );
+      $scope.credentials = {}
+    }
+
     $scope.createBlog = function() {
     	Blogs.create($scope.blog).success(function(data, status, headers, config) {
-    		$scope.entries.push(data);
+    		console.log('luoto')
+        $scope.entries.push(data);
     	});
-        $scope.flash = "A new blog entry '"+$scope.blog.subject+"'' created"
+      $scope.flash = "A new blog entry '"+$scope.blog.subject+"'' created"
     	$scope.formVisible = false;
     	$scope.blog = {}
     }
